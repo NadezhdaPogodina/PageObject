8dca89ca32e15b58c4f6536b36db73d6c42827c2
NadezhdaPogodina <pogodinadia@inbox.ru>
2023-09-01 12:57:43 +0300

    Change src/

--- src/test/java/ru/netology/data/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/data/test/MoneyTransferTest.java
@@ -52,8 +52,7 @@ public class MoneyTransferTest {
         var amount = generateInvalidAmount(secondCardBalance);
         var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
         transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
-        // transferPage.findErrorMessage();
-        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        transferPage.findErrorMessage("Выполнена попытка перевода суммы, превышающей остаток на карте списания");
         var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
         var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
         assertEquals(firstCardBalance, actualBalanceFirstCard);
